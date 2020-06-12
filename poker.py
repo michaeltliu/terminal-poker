cb48f3134e5677e17a3d58e03da0cc8d7448fa16
Michael Liu <40892553+michaeltliu@users.noreply.github.com>
2020-06-11 22:50:05 -0700

    work

--- poker.py
+++ poker.py
@@ -4,14 +4,26 @@ class Table:
     def __init__(self):
         self.players = []
         self.players.append(Human(self))
-        for i in range(20):
+        for i in range(8):
             self.players.append(CPU(self))
-        self.deck = list(range(1,53))
+            
+        self.deck = list(range(52))
         self.community = []
         self.turn = 0
         self.button = 0
         self.callAmt = 0
         self.potSize = 0
+        self.street = 0
+
+    def newHand(self):
+        self.deck = list(range(52))
+        self.community = []
+        self.turn = 0
+        self.button += 1
+        self.button %= len(self.players)
+        self.callAmt = 0
+        self.potSize = 0
+        self.street = 0
         
     def next(self):
         self.turn += 1
@@ -25,23 +37,28 @@ class Table:
                 self.deck.remove(i)
 
     def dealFlop(self):
-        flop = random.sample(self.deck, 3)
+        #flop = random.sample(self.deck, 3)
+        flop = [0,12,11]
         self.community.extend(flop)
         for i in flop:
             self.deck.remove(i)
+        self.street = 1
 
     def dealTurn(self):
-        turn = random.sample(self.deck, 1)
+        #turn = random.sample(self.deck, 1)
+        turn = [10]
         self.community.extend(turn)
         for i in turn:
             self.deck.remove(i)
+        self.street = 2
 
     def dealRiver(self):
         river = random.sample(self.deck, 1)
         self.community.extend(river)
         for i in river:
             self.deck.remove(i)
-        
+        self.street = 3
+                
 class Player:
     def __init__(self, table):
         self.table = table
@@ -87,14 +104,105 @@ class Human(Player):
             fold()
         
 class Toolkit():
-    def __init__(self, table):
+    def __init__(self, table, player):
         self.table = table
-    def pair():
+        self.player = player
+        self.hand = player.hand
+
+        self.rankFreq = {}
+        for i in self.hand:
+            self.rankFreq[i % 13] = self.rankFreq.get(i % 13, 0) + 1
+        for i in self.table.community:
+            self.rankFreq[i % 13] = self.rankFreq.get(i % 13, 0) + 1
+
+    def highCard(self):
+        return max(self.hand + self.table.community)
+
+    def pair(self):
+        pairs = []
+        for i in self.rankFreq.keys():
+            if self.rankFreq.get(i, 0) == 2:
+                pairs.append(i)
+
+        #if len(pairs) == 0: return None
+        return pairs
+
+    def twoPair(self):
+        pairs = []
+        for i in self.rankFreq.keys():
+            if self.rankFreq.get(i, 0) == 2:
+                pairs.append(i)
+
+        if len(pairs) == 0: return None
+
+        l = []
+        x = max(pairs)
+        l.append(x)
+        pairs.remove(x)
+
+        if len(pairs) == 0: return None
+        
+        l.append(max(pairs))
+        return l
+
+    def threeKind(self):
+        trips = []
+        for i in self.rankFreq.keys():
+            if self.rankFreq.get(i, 0) == 3:
+                trips.append(i)
+
+        #if len(trips) == 0: return None
+        return trips
+
+    def straight(self):
+        a, b = 13, 12
+        while a - b < 5:
+            if self.rankFreq.get(a % 13, 0) == 0:
+                a = b
+                b = a - 1
+            elif self.rankFreq.get(b, 0) > 0:
+                b -= 1
+            elif self.rankFreq.get(b, 0) == 0:
+                a = b - 1
+                b = a - 1
+
+            if a < 0:
+                return None
+        return [b + 1, a]
+
+    def flush(self):
+        suit = {}
+        for i in self.hand:
+            suit[int(i/13)] = suit.get(int(i/13), 0) + 1
+        for i in self.table.community:
+            suit[int(i/13)] = suit.get(int(i/13), 0) + 1
+
+        for s in suit.keys():
+            if suit.get(s, 0) == 5:
+                return s
+
+        return None
+
+    def house(self):
         pass
 
+    def fourKind(self):
+        quads = []
+        for i in self.rankFreq.keys():
+            if self.rankFreq.get(i, 0) == 4:
+                quads.append(i)
+
+        if len(quads) == 0: return None
+        return max(quads)
+
+
 table = Table()
+table.dealFlop()
+table.dealTurn()
 table.dealPlayers()
+table.dealRiver()
+print(table.community)
 for player in table.players:
     print(player.hand)
-table.dealFlop()
-print(table.community)
+    kit = Toolkit(table, player)
+    print(kit.highCard(), kit.pair(), kit.twoPair(), kit.threeKind(), kit.straight(), kit.flush())
